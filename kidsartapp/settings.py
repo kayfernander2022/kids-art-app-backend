b035f9261d941f8ce21cddfdf9d1ae90afb2716a
kayfernander2022 <kayfernander@hotmail.com>
2023-02-20 18:41:17 -0500

    updates to settings

--- kidsartapp/settings.py
+++ kidsartapp/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import os
 
 # Actual directory user files go to
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+#MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
 # URL used to access the media
 MEDIA_URL = '/media/'
 
